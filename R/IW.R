fcb1cb5ce2b20ccca2d05cf3f4ae41ca93b904c8
Olga Usuga <ousuga@gmail.com>
2017-08-05 11:08:44 -0500

    Cambio de parametros

--- R/IW.R
+++ R/IW.R
@@ -6,22 +6,22 @@
 #' @description 
 #' Density, distribution function, quantile function, 
 #' random generation and hazard function for the inverse weibull distribution with
-#' parameters \code{alpha} and \code{theta}.
+#' parameters \code{mu} and \code{sigma}.
 #' 
 #' @param x,q	vector of quantiles.
 #' @param p vector of probabilities.
 #' @param n number of observations. 
-#' @param alpha parameter one.
-#' @param theta parameter two.   
+#' @param mu parameter one.
+#' @param sigma parameter two.   
 #' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
 #' @param lower.tail logical; if TRUE (default), probabilities are 
 #' P[X <= x], otherwise, P[X > x].
 #' 
 #' @details 
-#' The inverse weibull distribution with parameters \code{alpha} and
-#' \code{theta} has density given by
+#' The inverse weibull distribution with parameters \code{mu} and
+#' \code{sigma} has density given by
 #' 
-#' f(x) = alpha*theta*x^(-theta-1)*exp(-alpha*(x^-theta)),
+#' f(x) = mu*sigma*x^(-sigma-1)*exp(-mu*(x^-sigma)),
 #' 
 #' for x > 0.
 #' 
@@ -33,35 +33,82 @@
 #' @export
 #' @examples  
 #' ## The probability density function 
-#' curve(dIW(x, alpha = 5, theta = 2.5), from = 0, to = 10, ylim = c(0, 0.55), col = "red", las = 1, ylab = "The probability density function")
+#' curve(dIW(x, mu = 5, sigma = 2.5), from = 0, to = 10, ylim = c(0, 0.55), col = "red", las = 1, ylab = "The probability density function")
 #' 
 #' ## The cumulative distribution and the Reliability function
 #' par(mfrow = c(1, 2))
-#' curve(pIW(x, alpha = 5, theta = 2.5), from = 0, to = 10, ylim = c(0, 1), col = "red", las = 1, ylab = "The cumulative distribution function")
-#' curve(pIW(x, alpha = 5, theta = 2.5, lower.tail = FALSE), from = 0, to = 10, ylim = c(0, 1), col = "red", las = 1, ylab = "The Reliability function")
+#' curve(pIW(x, mu = 5, sigma = 2.5), from = 0, to = 10, ylim = c(0, 1), col = "red", las = 1, ylab = "The cumulative distribution function")
+#' curve(pIW(x, mu = 5, sigma = 2.5, lower.tail = FALSE), from = 0, to = 10, ylim = c(0, 1), col = "red", las = 1, ylab = "The Reliability function")
 #' 
 #' ## The quantile function
 #' p <- seq(from = 0, to = 0.998, length.out = 100)
-#' plot(x = qIW(p, alpha = 5, theta = 2.5), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
-#' curve(pIW(x, alpha = 5, theta = 2.5),  from = 0, add = TRUE, col = "red")
+#' plot(x = qIW(p, mu = 5, sigma = 2.5), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
+#' curve(pIW(x, mu = 5, sigma = 2.5),  from = 0, add = TRUE, col = "red")
 #' 
 #' ## The random function
-#' hist(rIW(n = 1000, alpha = 5, theta = 2.5), freq = FALSE, xlab = "x", ylim = c(0, 0.55), las = 1, main = "")
-#' curve(dIW(x, alpha = 5, theta = 2.5),  from = 0, to = 10, add = TRUE, ylim = c(0, 0.55), col = "red")
+#' hist(rIW(n = 1000, mu = 5, sigma = 2.5), freq = FALSE, xlab = "x", ylim = c(0, 0.55), las = 1, main = "")
+#' curve(dIW(x, mu = 5, sigma = 2.5),  from = 0, to = 10, add = TRUE, ylim = c(0, 0.55), col = "red")
 #' 
 #' ## The Hazard function
-#' curve(hIW(x, alpha = 5, theta = 2.5), from = 0, to = 15, ylim = c(0, 1), col = "red", las = 1, ylab = "The Hazard function")
-
-dIW<-function(x,alpha,theta, log = FALSE){
+#' curve(hIW(x, mu = 5, sigma = 2.5), from = 0, to = 15, ylim = c(0, 1), col = "red", las = 1, ylab = "The Hazard function")
+IW <- function (mu.link="log", sigma.link="log") 
+{
+  mstats <- checklink("mu.link", "Inverse Weibull", substitute(mu.link), c("log", "own"))
+  dstats <- checklink("sigma.link", "Inverse Weibull", substitute(sigma.link), c("log", "own"))
+  
+  structure(list(family = c("IW", "Inverse Weibull"),
+                 parameters = list(mu=TRUE, sigma=TRUE), 
+                 nopar = 2, 
+                 type = "Continuous",
+                 
+                 mu.link = as.character(substitute(mu.link)), 
+                 sigma.link = as.character(substitute(sigma.link)), 
+                 
+                 mu.linkfun = mstats$linkfun, 
+                 sigma.linkfun = dstats$linkfun, 
+                 
+                 mu.linkinv = mstats$linkinv, 
+                 sigma.linkinv = dstats$linkinv,
+                 
+                 mu.dr = mstats$mu.eta, 
+                 sigma.dr = dstats$mu.eta,
+                 
+                 dldm = function(y,mu,sigma) (1/mu)-y^(-sigma),
+                 d2ldm2 = function(mu) -1/(mu^2),
+                 dldd = function(y,mu,sigma) (1/sigma)-log(y) +mu*(y^(-sigma))*log(y),
+                 d2ldd2 = function(y,mu,sigma) {
+                   dldd = function(y,mu,sigma) -(1/(sigma^2))-mu*((log(y))^2)*(y^(-sigma))
+                   ans <- dldd(y,mu,sigma)
+                   ans <- -ans^2
+                 },
+                 
+                 d2ldmdd = function(y,sigma) -(log(y)*(y^(-sigma)))^2,
+                 
+                 G.dev.incr  = function(y,mu,sigma,...) -2*dIW(y, mu, sigma, log=TRUE), 
+                 rqres = expression(rqres(pfun="pIW", type="Continuous", y=y, mu=mu, sigma=sigma)),
+                 
+                 mu.initial = expression( mu <-  rep(0.5, length(y)) ),     
+                 sigma.initial = expression( sigma <- rep(0.5, length(y)) ),
+                 
+                 mu.valid = function(mu) all(mu > 0) , 
+                 sigma.valid = function(sigma)  all(sigma > 0), 
+                 
+                 y.valid = function(y)  all(y > 0)
+  ),
+  class = c("gamlss.family","family"))
+}
+#' @export
+#' @rdname IW
+dIW<-function(x,mu,sigma, log = FALSE){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
-  if (any(alpha <= 0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(theta<=0)) 
-    stop(paste("theta must be positive", "\n", ""))
+  if (any(mu <= 0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
-  loglik<- log(alpha) + log(theta) - (theta+1)*log(x) - 
-    alpha*(x^-theta)
+  loglik<- log(mu) + log(sigma) - (sigma+1)*log(x) - 
+    mu*(x^-sigma)
   
   if (log == FALSE) 
     density<- exp(loglik)
@@ -72,15 +119,15 @@ dIW<-function(x,alpha,theta, log = FALSE){
 
 #' @export
 #' @rdname IW
-pIW <- function(q,alpha,theta, lower.tail=TRUE, log.p = FALSE){
+pIW <- function(q,mu,sigma, lower.tail=TRUE, log.p = FALSE){
   if (any(q<0)) 
     stop(paste("q must be positive", "\n", ""))
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(theta<=0)) 
-    stop(paste("theta must be positive", "\n", ""))
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
-  cdf <- exp((-alpha)*(q^(-theta)))
+  cdf <- exp((-mu)*(q^(-sigma)))
   
   if (lower.tail == TRUE) 
     cdf <- cdf
@@ -93,11 +140,11 @@ pIW <- function(q,alpha,theta, lower.tail=TRUE, log.p = FALSE){
 
 #' @export
 #' @rdname IW
-qIW <- function(p,alpha,theta, lower.tail = TRUE, log.p = FALSE){
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(theta<=0)) 
-    stop(paste("theta must be positive", "\n", ""))
+qIW <- function(p,mu,sigma, lower.tail = TRUE, log.p = FALSE){
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
   if (log.p == TRUE) 
     p <- exp(p)
@@ -109,37 +156,37 @@ qIW <- function(p,alpha,theta, lower.tail = TRUE, log.p = FALSE){
   if (any(p < 0) | any(p > 1)) 
     stop(paste("p must be between 0 and 1", "\n", ""))
   
-  q <- ((-1/alpha)*log(p))^(-1/theta)
+  q <- ((-1/mu)*log(p))^(-1/sigma)
   q
 }
 
 #' @export
 #' @rdname IW
-rIW <- function(n,alpha,theta){
+rIW <- function(n,mu,sigma){
   if(any(n<=0))
     stop(paste("n must be positive","\n",""))
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(theta<=0)) 
-    stop(paste("theta must be positive", "\n", ""))
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
   n <- ceiling(n)
   p <- runif(n)
-  r <- qIW(p, alpha,theta)
+  r <- qIW(p, mu,sigma)
   r
 }
 
 #' @export
 #' @rdname IW
-hIW<-function(x,alpha,theta){
+hIW<-function(x,mu,sigma){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
-  if (any(alpha <= 0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(theta<=0)) 
-    stop(paste("theta must be positive", "\n", ""))
+  if (any(mu <= 0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
-  h <- dIW(x,alpha,theta, log = FALSE)/pIW(q=x,alpha,theta, lower.tail=FALSE, log.p = FALSE)
+  h <- dIW(x,mu,sigma, log = FALSE)/pIW(q=x,mu,sigma, lower.tail=FALSE, log.p = FALSE)
   h
 }
 
