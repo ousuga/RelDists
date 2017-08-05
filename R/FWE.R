fd213170e65053bda6126b17b4688ac2507c953d
Olga Usuga <ousuga@gmail.com>
2017-08-05 10:28:41 -0500

    Cambio de parametros

--- R/FWE.R
+++ R/FWE.R
@@ -6,22 +6,22 @@
 #' @description 
 #' Density, distribution function, quantile function, 
 #' random generation  and hazard function for the flexible weibull extension distribution with
-#' parameters \code{alpha} and  \code{beta}.
+#' parameters \code{mu} and  \code{sigma}.
 #' 
 #' @param x,q	vector of quantiles.
 #' @param p vector of probabilities.
 #' @param n number of observations. 
-#' @param alpha parameter.    
-#' @param beta parameter.
+#' @param mu parameter.    
+#' @param sigma parameter.
 #' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
 #' @param lower.tail logical; if TRUE (default), probabilities are 
 #' P[X <= x], otherwise, P[X > x].
 #' 
 #' @details 
-#' The flexible weibull extension with parameters \code{alpha} and \code{beta}
+#' The flexible weibull extension with parameters \code{mu} and \code{sigma}
 #' has density given by
 #' 
-#' f(x) = (alpha + (beta/x^2))*exp(alpha*x - beta/x)*exp(-exp(alpha*x-beta/x))
+#' f(x) = (mu + (sigma/x^2))*exp(mu*x - sigma/x)*exp(-exp(mu*x-sigma/x))
 #' 
 #' for x>0.
 #' 
@@ -33,35 +33,35 @@
 #' @export
 #' @examples  
 #' ## The probability density function
-#' curve(dFWE(x, alpha = 0.75, beta = 0.5), from = 0, to = 3, ylim = c(0, 1.7), col = "red", las = 1, ylab = "The probability density function")
+#' curve(dFWE(x, mu = 0.75, sigma = 0.5), from = 0, to = 3, ylim = c(0, 1.7), col = "red", las = 1, ylab = "The probability density function")
 #' 
 #' ## The cumulative distribution and the Reliability function
 #' par(mfrow = c(1, 2))
-#' curve(pFWE(x, alpha = 0.75, beta = 0.5), from = 0, to = 3, col = "red", las = 1, ylab = "The cumulative distribution function")
-#' curve(pFWE(x, alpha = 0.75, beta = 0.5, lower.tail = FALSE), from = 0, to = 3, col = "red", las = 1, ylab = "The Reliability function")
+#' curve(pFWE(x, mu = 0.75, sigma = 0.5), from = 0, to = 3, col = "red", las = 1, ylab = "The cumulative distribution function")
+#' curve(pFWE(x, mu = 0.75, sigma = 0.5, lower.tail = FALSE), from = 0, to = 3, col = "red", las = 1, ylab = "The Reliability function")
 #' 
 #' ## The quantile function
 #' p <- seq(from = 0, to = 0.99999, length.out = 100)
-#' plot(x = qFWE(p,alpha = 0.75, beta = 0.5), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
-#' curve(pFWE(x, alpha = 0.75, beta = 0.5), from = 0, add = TRUE, col = "red")
+#' plot(x = qFWE(p,mu = 0.75, sigma = 0.5), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
+#' curve(pFWE(x, mu = 0.75, sigma = 0.5), from = 0, add = TRUE, col = "red")
 #'  
 #' ## The random function
-#' hist(rFWE(n = 1000, alpha = 2, beta = 0.5), freq = FALSE, xlab = "x", ylim = c(0, 2), las = 1, main = "")
-#' curve(dFWE(x, alpha = 2, beta = 0.5),  from = 0, to = 3, add = TRUE, , col = "red")
+#' hist(rFWE(n = 1000, mu = 2, sigma = 0.5), freq = FALSE, xlab = "x", ylim = c(0, 2), las = 1, main = "")
+#' curve(dFWE(x, mu = 2, sigma = 0.5),  from = 0, to = 3, add = TRUE, , col = "red")
 #' 
 #' ## The Hazard function
-#' curve(hFWE(x, alpha = 0.75, beta = 0.5), from = 0, to = 2, ylim = c(0, 2.5), col = "red", ylab = "The Hazard function", las = 1)
+#' curve(hFWE(x, mu = 0.75, sigma = 0.5), from = 0, to = 2, ylim = c(0, 2.5), col = "red", ylab = "The Hazard function", las = 1)
 #' 
-dFWE<-function(x,alpha,beta,log = FALSE){
+dFWE<-function(x,mu,sigma,log = FALSE){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(beta<=0)) 
-    stop(paste("beta must be positive", "\n", ""))
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
-  loglik<- log(alpha + (beta/x^2)) + (alpha*x) - (beta/x) - 
-    exp(alpha*x - (beta/x))
+  loglik<- log(mu + (sigma/x^2)) + (mu*x) - (sigma/x) - 
+    exp(mu*x - (sigma/x))
   
   if (log == FALSE) 
     density<- exp(loglik)
@@ -71,15 +71,15 @@ dFWE<-function(x,alpha,beta,log = FALSE){
 }
 #' @export
 #' @rdname FWE
-pFWE <- function(q,alpha,beta, lower.tail=TRUE, log.p = FALSE){
+pFWE <- function(q,mu,sigma, lower.tail=TRUE, log.p = FALSE){
   if (any(q<0)) 
     stop(paste("q must be positive", "\n", ""))
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(beta<=0)) 
-    stop(paste("beta must be positive", "\n", ""))
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
-  cdf <- 1- exp(-exp(alpha*q - beta/q))
+  cdf <- 1- exp(-exp(mu*q - sigma/q))
   
   if (lower.tail == TRUE) 
     cdf <- cdf
@@ -92,11 +92,11 @@ pFWE <- function(q,alpha,beta, lower.tail=TRUE, log.p = FALSE){
 }
 #' @export
 #' @rdname FWE
-qFWE <- function(p, alpha, beta, lower.tail = TRUE, log.p = FALSE) {
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(beta<=0)) 
-    stop(paste("beta must be positive", "\n", ""))
+qFWE <- function(p, mu, sigma, lower.tail = TRUE, log.p = FALSE) {
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
   if (log.p == TRUE) 
     p <- exp(p)
@@ -107,46 +107,46 @@ qFWE <- function(p, alpha, beta, lower.tail = TRUE, log.p = FALSE) {
   if (any(p < 0) | any(p > 1)) 
     stop(paste("p must be between 0 and 1", "\n", ""))
   
-  fda <- function(x,alpha, beta){
-    1- exp(-exp(alpha*x - beta/x))
+  fda <- function(x,mu, sigma){
+    1- exp(-exp(mu*x - sigma/x))
   }
   
-  fda1 <- function(x, alpha, beta, p) {fda(x, alpha, beta) - p}
+  fda1 <- function(x, mu, sigma, p) {fda(x, mu, sigma) - p}
   
-  r_de_la_funcion <- function(alpha, beta, p) {
-    uniroot(fda1, interval=c(0,1e+06), alpha, beta, p)$root
+  r_de_la_funcion <- function(mu, sigma, p) {
+    uniroot(fda1, interval=c(0,1e+06), mu, sigma, p)$root
   }
   
   r_de_la_funcion <- Vectorize(r_de_la_funcion)
-  q <- r_de_la_funcion(alpha, beta, p)
+  q <- r_de_la_funcion(mu, sigma, p)
   q
   
 }
 
 #' @export
 #' @rdname FWE
-rFWE <- function(n,alpha,beta){
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(beta<=0)) 
-    stop(paste("beta must be positive", "\n", ""))  
+rFWE <- function(n,mu,sigma){
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))  
   
   n <- ceiling(n)
   p <- runif(n)
-  r <- qFWE(p, alpha,beta)
+  r <- qFWE(p, mu,sigma)
   r
 }
 #' @export
 #' @rdname FWE
-hFWE<-function(x,alpha,beta){
+hFWE<-function(x,mu,sigma){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
-  if (any(alpha<=0 )) 
-    stop(paste("alpha must be positive", "\n", ""))
-  if (any(beta<=0)) 
-    stop(paste("beta must be positive", "\n", ""))
+  if (any(mu<=0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma<=0)) 
+    stop(paste("sigma must be positive", "\n", ""))
   
-  h <- dFWE(x,alpha,beta, log = FALSE)/pFWE(q=x,alpha,beta, lower.tail=FALSE, log.p = FALSE)
+  h <- dFWE(x,mu,sigma, log = FALSE)/pFWE(q=x,mu,sigma, lower.tail=FALSE, log.p = FALSE)
   h
 }
 
