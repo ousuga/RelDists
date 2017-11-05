25b56697b09c3131ebfc3928fc02cbbab3c05196
fhernanb <fhernanb@unal.edu.co>
2017-11-05 17:08:06 -0500

    cambios en el archivo de ejemplos

--- example_with_FWE.R
+++ example_with_FWE.R
@@ -1,3 +1,6 @@
+require(RelDists)
+require(gamlss)
+
 
 ## Estimating parameters
 mu <- 0.5     # Fixing mu
@@ -18,6 +21,10 @@ x2 <- rpois(n, lambda=3)
 mu    <- exp(-1 + 3.5 * x1)
 sigma <- exp(-4.9 + 2 * x2)
 y <- rFWE(n=n, mu=mu, sigma=sigma)
+mod0 <- gamlss(y ~ 1, sigma.fo= ~ 1, family=FWE)
+exp(coef(mod0, "mu"))
+exp(coef(mod0, "sigma"))
+
 mod <- NULL
 mod <- gamlss(y ~ x1, sigma.fo= ~ x2, family=FWE,
               control=gamlss.control(n.cyc=20, trace=FALSE),
