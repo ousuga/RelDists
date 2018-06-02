5672b6c2598a3af5cae07e397f4ac98d66549a6a
fhernanb <fhernanb@unal.edu.co>
2018-06-01 22:55:26 -0500

    se incluyo p

--- Pruebas/WGEE.R
+++ Pruebas/WGEE.R
@@ -25,6 +25,10 @@ dWGEE <- function(x, mu, sigma, nu, log=FALSE) {
   return(density)
 }
 
+#---------------------------------------------------------------------
+
+integrate(dWGEE, lower=0, upper=Inf,
+          mu=5, sigma=0.5, nu=1)
 
 # Figure 3
 curve(dWGEE(x, mu=5, sigma=0.5, nu=1),
@@ -33,5 +37,44 @@ curve(dWGEE(x, mu=1, sigma=0.5, nu=1),
       from=0, to=6, ylab='f(x)', las=1, add=TRUE)
 curve(dWGEE(x, mu=0.1, sigma=0.5, nu=1),
       from=0, to=6, ylab='f(x)', las=1, add=TRUE)
+
+# The incomplete beta function
+ibeta <- function(x, a, b) {pbeta(x, a, b, lower.tail=F) * beta(a, b)}
+
+pWGEE <- function(q, mu, sigma, nu, lower.tail=TRUE, log.p=FALSE){
+  if (any(q < 0)) 
+    stop(paste("q must be positive", "\n", ""))
+  if (any(mu <= 0 )) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", ""))
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", ""))
+  
+  # To convert mu, sigma, nu and tau to original parameters
+  alp <- mu
+  bet <- sigma
+  lam <- nu
+  
+  cdf <- ((1-exp(-lam*q))^bet - bet * ibeta(exp(-lam*q), alp+1, bet)) / 
+    (1 - bet * beta(alp + 1, bet))
+  if (lower.tail == TRUE) 
+    cdf <- cdf
+  else cdf <- 1 - cdf
+  if (log.p == FALSE) 
+    cdf <- cdf
+  else cdf <- log(cdf)
+  cdf 
+}
+
+curve(pWGEE(x, mu=5, sigma=0.5, nu=1),
+      from=0, to=6, ylab='F(x)', las=1)
+
+# Test
+integrate(dWGEE, lower=0, upper=1,
+          mu=5, sigma=0.5, nu=1)
+
+pWGEE(q=1, mu=5, sigma=0.5, nu=1)
+
 
 
