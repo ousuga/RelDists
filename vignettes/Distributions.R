250757ef5143ef45ce802f2c8ef1595002d1c0fd
ousuga <ousuga@gmail.com>
2018-11-15 16:24:53 -0500

    cambios en vignete

---
+++ vignettes/Distributions.R
@@ -0,0 +1,27 @@
+[![Licence](https://img.shields.io/badge/licence-GPL--3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0.en.html)
+---
+  title: "Distributions"
+author: "Olga Usuga, Freddy Hernández, Catherine Rincón y Mauricio Barrientos"
+date: "`r Sys.Date()`"
+output: 
+  pdf_document: 
+  number_sections: yes
+toc: yes
+vignette: >
+  %\VignetteIndexEntry{hola}
+%\VignetteEngine{knitr::rmarkdown}
+%\VignetteEncoding{UTF-8}
+---
+  
+  ```{r setup, include=FALSE}
+require(RelDists)
+knitr::opts_chunk$set(echo = FALSE)
+```
+
+# Exponentiated Weibull distribution
+
+## Probability density function 
+The exponentiated Weibull distribution (EW) has three parameters. The pdf of the EW distribution is:
+RelDists <img src="man/figures/RelDists4.3_gris.png" align="right" height="200" align="right"/>
+  
+  [![Travis build status](https://travis-ci.org/ousuga/RelDists.svg?branch=master)](https://travis-ci.org/ousuga/RelDists)
