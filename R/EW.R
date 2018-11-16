085da3daf80ddad86b064b7e2e4c24944618da4a
ousuga <ousuga@gmail.com>
2018-11-15 23:04:03 -0500

    Cambios en ayuda

--- R/EW.R
+++ R/EW.R
@@ -1,24 +1,14 @@
 #' @name EW
 #' 
 #' @title 
-#' The Exponentiated Weibull Distribution
+#' The Exponentiated Weibull family
 #' 
 #' @description 
-#' Density, distribution function, quantile function, 
-#' random generation  and hazard function for the exponentiated weibull distribution with
-#' parameters \code{mu}, \code{sigma} and \code{nu}.
+#' The Exponentiated Weibull family
 #' 
 #' @param mu.link defines the mu.link, with "log" link as the default for the mu parameter.
 #' @param sigma.link defines the sigma.link, with "log" link as the default for the sigma.
 #' @param nu.link defines the nu.link, with "log" link as the default for the nu parameter.
-#' @param x,q	vector of quantiles.
-#' @param p vector of probabilities.
-#' @param n number of observations. 
-#' @param mu scale parameter.
-#' @param sigma,nu shape parameters.
-#' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
-#' @param lower.tail logical; if TRUE (default), probabilities are 
-#' P[X <= x], otherwise, P[X > x].
 #' 
 #' @details 
 #' The Exponentiated Weibull Distribution with parameters \code{mu}, 
@@ -33,33 +23,8 @@
 #' function, \code{qEW} gives the quantile function, \code{rEW}
 #' generates random deviates and \code{hEW} gives the hazard function.
 #'
+#' @importFrom gamlss checklink rqres
 #' @export
-#' @examples  
-#' ## The probability density function
-#' curve(dEW(x, mu=2, sigma=1.5, nu=0.5), from=0, to=2,
-#'       ylim=c(0, 2.5), col="red", las=1, ylab="The probability density function") 
-#' 
-#' ## The cumulative distribution and the Reliability function
-#' par(mfrow=c(1, 2))
-#' curve(pEW(x, mu=2, sigma=1.5, nu=0.5), from=0, to=2,  col="red",
-#'      las=1, ylab="The cumulative distribution function")
-#' curve(pEW(x, mu=2, sigma=1.5, nu=0.5, lower.tail=FALSE), from=0,
-#'       to=2,  col="red", las= , ylab="The Reliability function")
-#' 
-#' ## The quantile function
-#' p <- seq(from=0, to=0.99999, length.out= 00)
-#' plot(x=qEW(p, mu=2, sigma=1.5, nu=0.5), y=p, xlab="Quantile", las=1, ylab="Probability")
-#' curve(pEW(x, mu=2, sigma=1.5, nu=0.5),  from=0, add=TRUE, col="red")
-#' 
-#' ## The random function
-#' hist(rEW(n=10000, mu=2, sigma=1.5, nu=0.5), freq=FALSE, xlab="x", las=1, main="")
-#' curve(dEW(x, mu=2, sigma=1.5, nu=0.5),  from=0, add=TRUE, col="red") 
-#' 
-#' ## The Hazard function
-#' curve(hEW(x, mu=2,sigma=1.5, nu=0.5), from=0, to=2, ylim=c(0, 7), col="red",
-#'       ylab="The Hazard function")
-#' 
-#' @importFrom gamlss checklink
 EW <- function (mu.link="log", sigma.link="log", nu.link="log") 
 {
   mstats <- checklink("mu.link",    "Exponentiated Weibull", 
@@ -188,8 +153,62 @@ EW <- function (mu.link="log", sigma.link="log", nu.link="log")
   ), 
   class=c("gamlss.family", "family"))
 }
+#' 
+#' #' @title 
+#' The Exponentiated Weibull Distribution
+#' 
+#' @description 
+#' Density, distribution function, quantile function, 
+#' random generation  and hazard function for the exponentiated weibull distribution with
+#' parameters \code{mu}, \code{sigma} and \code{nu}.
+#' 
+#' @param x,q	vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations. 
+#' @param mu scale parameter.
+#' @param sigma,nu shape parameters.
+#' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
+#' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
+#' 
+#' #' @details 
+#' The Exponentiated Weibull Distribution with parameters \code{mu}, 
+#' \code{sigma} and \code{nu} has density given by
+#' 
+#' \eqn{f(x)=\nu \mu \sigma x^{\sigma-1} \exp(-\mu x^\sigma) (1-\exp(-\mu x^\sigma))^{\nu-1},}
+#' 
+#' for x > 0. 
+#' 
+#' @return 
+#' \code{dEW} gives the density, \code{pEW} gives the distribution 
+#' function, \code{qEW} gives the quantile function, \code{rEW}
+#' generates random deviates and \code{hEW} gives the hazard function.
+#'
+#' @examples  
+#' ## The probability density function
+#' curve(dEW(x, mu=2, sigma=1.5, nu=0.5), from=0, to=2,
+#'       ylim=c(0, 2.5), col="red", las=1, ylab="The probability density function") 
+#' 
+#' ## The cumulative distribution and the Reliability function
+#' par(mfrow=c(1, 2))
+#' curve(pEW(x, mu=2, sigma=1.5, nu=0.5), from=0, to=2,  col="red",
+#'      las=1, ylab="The cumulative distribution function")
+#' curve(pEW(x, mu=2, sigma=1.5, nu=0.5, lower.tail=FALSE), from=0,
+#'       to=2,  col="red", las= , ylab="The Reliability function")
+#' 
+#' ## The quantile function
+#' p <- seq(from=0, to=0.99999, length.out= 00)
+#' plot(x=qEW(p, mu=2, sigma=1.5, nu=0.5), y=p, xlab="Quantile", las=1, ylab="Probability")
+#' curve(pEW(x, mu=2, sigma=1.5, nu=0.5),  from=0, add=TRUE, col="red")
+#' 
+#' ## The random function
+#' hist(rEW(n=10000, mu=2, sigma=1.5, nu=0.5), freq=FALSE, xlab="x", las=1, main="")
+#' curve(dEW(x, mu=2, sigma=1.5, nu=0.5),  from=0, add=TRUE, col="red") 
+#' 
+#' ## The Hazard function
+#' curve(hEW(x, mu=2,sigma=1.5, nu=0.5), from=0, to=2, ylim=c(0, 7), col="red",
+#'       ylab="The Hazard function")
+#' 
 #' @export
-#' @rdname EW
 dEW<-function(x,mu,sigma,nu, log=FALSE){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
@@ -211,7 +230,7 @@ dEW<-function(x,mu,sigma,nu, log=FALSE){
 }
 
 #' @export
-#' @rdname EW
+#' @rdname dEW
 pEW <- function(q,mu,sigma,nu, lower.tail=TRUE, log.p=FALSE){
   if (any(q<0)) 
     stop(paste("q must be positive", "\n", ""))
@@ -233,7 +252,7 @@ pEW <- function(q,mu,sigma,nu, lower.tail=TRUE, log.p=FALSE){
 }
 
 #' @export
-#' @rdname EW
+#' @rdname dEW
 qEW <- function(p,mu,sigma,nu, lower.tail=TRUE, log.p=FALSE){
   if (any(mu<=0 )) 
     stop(paste("mu must be positive", "\n", ""))
@@ -256,7 +275,7 @@ qEW <- function(p,mu,sigma,nu, lower.tail=TRUE, log.p=FALSE){
 }
 #' @importFrom stats runif
 #' @export
-#' @rdname EW
+#' @rdname dEW
 rEW <- function(n,mu,sigma, nu){
   if(any(n<=0))
     stop(paste("n must be positive","\n",""))
@@ -273,7 +292,7 @@ rEW <- function(n,mu,sigma, nu){
   r
 }
 #' @export
-#' @rdname EW
+#' @rdname dEW
 hEW<-function(x,mu,sigma, nu){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
