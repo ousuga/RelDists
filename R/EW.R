296b4a5fc11a3dd99cbb8f38817ed7c8d4c02f31
fhernanb <fhernanb@unal.edu.co>
2018-11-19 14:51:55 -0500

    se arregló @details

--- R/EW.R
+++ R/EW.R
@@ -13,7 +13,7 @@
 #' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
 #' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
 #' 
-#' #' @details 
+#' @details 
 #' The Exponentiated Weibull Distribution with parameters \code{mu}, 
 #' \code{sigma} and \code{nu} has density given by
 #' 
