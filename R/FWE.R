399fd597708ea86e83fc7e0bddd92655b4cb7108
fhernanb <fhernanb@unal.edu.co>
2018-12-01 17:07:58 -0500

    se colocaron ejemplos para la familia FWE

--- R/FWE.R
+++ R/FWE.R
@@ -15,7 +15,35 @@
 #' for x>0.
 #' 
 #' @examples  
-#' plot(1:5)
+#' # Example 1
+#' # Generating some random values with
+#' # known mu and sigma
+#' y <- rFWE(n=100, mu=0.75, sigma=1.3)
+#' 
+#' # Fitting the model
+#' require(gamlss)
+#' mod <- gamlss(y~1, sigma.fo=~1, family='FWE',
+#'               control=gamlss.control(n.cyc=5000, trace=FALSE))
+#' 
+#' # Extracting the fitted values for mu and sigma
+#' # using the inverse link function
+#' exp(coef(mod, what='mu'))
+#' exp(coef(mod, what='sigma'))
+#' 
+#' # Example 2
+#' # Generating random values under some model
+#' n <- 200
+#' x1 <- runif(n)
+#' x2 <- runif(n)
+#' mu <- exp(1.21 - 3 * x1)
+#' sigma <- exp(1.26 - 2 * x2)
+#' x <- rFWE(n=n, mu, sigma)
+#' 
+#' mod <- gamlss(x~x1, sigma.fo=~x2, family=FWE, 
+#'               control=gamlss.control(n.cyc=5000, trace=FALSE))
+#' 
+#' coef(mod, what="mu")
+#' coef(mod, what="sigma")
 #' 
 #' @importFrom gamlss.dist checklink
 #' @importFrom gamlss rqres.plot
