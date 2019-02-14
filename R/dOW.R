dd70d239ac3dfb41f5f7e7550a37ea32797a6fb8
Jaimemosg <jmosquerag@unal.edu.co>
2019-02-14 11:31:41 -0500

    Script actualizado con la nueva parametrización :fire:

--- R/dOW.R
+++ R/dOW.R
@@ -28,34 +28,27 @@
 #' 
 #' @examples  
 #' ## The probability density function 
-#' curve(dOW(x, mu=2, sigma=3, nu=0.2), from=0, to=4, ylim=c(0, 2), col="red",
-#'       las=1, ylab="The probability density function")
+#' curve(dOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, to = 4, ylim = c(0, 2), col = "red", las = 1, ylab = "The probability density function")
 #' 
 #' ## The cumulative distribution and the Reliability function
-#' par(mfrow=c(1, 2))
-#' curve(pOW(x, mu=2, sigma=3, nu=0.2), from=0, to=4, ylim=c(0, 1), col="red",
-#'       las=1, ylab="The cumulative distribution function")
-#' curve(pOW(x, mu=2, sigma=3, nu=0.2, lower.tail = FALSE), from = 0, to = 4,
-#'       ylim=c(0, 1), col="red", las=1, ylab="The Reliability function")
+#' par(mfrow = c(1, 2))
+#' curve(pOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, to = 4, ylim = c(0, 1), col = "red", las = 1, ylab = "The cumulative distribution function")
+#' curve(pOW(x, mu = 2, sigma = 3, nu = 0.2, lower.tail = FALSE), from = 0, to = 4,  ylim = c(0, 1), col = "red", las = 1, ylab = "The Reliability function")
 #' 
 #' ## The quantile function
-#' p <- seq(from=0, to=0.998, length.out=100)
-#' plot(x = qOW(p, mu=2, sigma=3, nu=0.2), y=p, xlab="Quantile", las=1, 
-#'      ylab="Probability")
-#' curve(pOW(x, mu=2, sigma=3, nu=0.2), from=0, add=TRUE, col="red")
+#' p <- seq(from = 0, to = 0.998, length.out = 100)
+#' plot(x = qOW(p, mu = 2, sigma = 3, nu = 0.2), y = p, xlab = "Quantile", las = 1, ylab = "Probability")
+#' curve(pOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, add = TRUE, col = "red")
 #' 
 #' ## The random function
-#' hist(rOW(n=10000, mu=2, sigma=3, nu=0.2), freq=FALSE, ylim=c(0, 2), 
-#'      xlab="x", las=1, main="")
-#' curve(dOW(x, mu=2, sigma=3, nu=0.2),  from=0, ylim=c(0, 2), add=TRUE,
-#'       col="red")
+#' hist(rOW(n = 10000, mu = 2, sigma = 3, nu = 0.2), freq = FALSE, ylim = c(0, 2),xlab = "x", las = 1, main = "")
+#' curve(dOW(x, mu = 2, sigma = 3, nu = 0.2),  from = 0, ylim = c(0, 2), add = TRUE, col = "red")
 #' 
 #' ## The Hazard function
-#' curve(hOW(x, mu=2, sigma=3, nu=0.2), from=0, to=2.5, ylim=c(0, 3), 
-#'       col="red", ylab="The hazard function", las=1)
+#' curve(hOW(x, mu = 2, sigma = 3, nu = 0.2), from = 0, to = 2.5, ylim = c(0, 3), col = "red", ylab = "The hazard function", las = 1)
 #'
 #' @export
-dOW<-function(x, mu, sigma, nu, log = FALSE){
+dOW<-function(x, mu, sigma, nu, log=FALSE){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
   if (any(mu<=0 )) 
@@ -63,20 +56,19 @@ dOW<-function(x, mu, sigma, nu, log = FALSE){
   if (any(sigma*nu<=0)) 
     stop(paste("Product sigma*nu must be positive", "\n", ""))
   
-  loglik<- log(mu) +log(sigma*nu) + (sigma-1)*log(x) +
-    mu*(x^sigma) + (nu-1)*log(exp(mu*(x^sigma))-1) -
-    2*log(1+(exp(mu*(x^sigma))-1)^nu)
+  loglik <- log(sigma*nu) - log(x) + sigma*( log(mu) + log(x) ) +
+    (mu*x)^sigma + ( nu-1 )*log(exp( (mu*x)^sigma ) - 1) -
+    2*log( 1 + (exp((mu*x)^sigma) - 1)^nu )
   
-  if (log == FALSE) 
+  if (log == FALSE)
     density<- exp(loglik)
   else 
     density <- loglik
   return(density)
 }
-
 #' @export
 #' @rdname dOW
-pOW <- function(q,mu,sigma,nu, lower.tail=TRUE, log.p = FALSE){
+pOW <- function(q, mu, sigma, nu, lower.tail=TRUE, log.p=FALSE){
   if (any(q<0)) 
     stop(paste("q must be positive", "\n", ""))
   if (any(mu<=0 )) 
@@ -84,33 +76,33 @@ pOW <- function(q,mu,sigma,nu, lower.tail=TRUE, log.p = FALSE){
   if (any(sigma*nu<=0)) 
     stop(paste("Product sigma*nu must be positive", "\n", ""))
   
-  cdf <- 1 - (1 + (exp(mu*(q^sigma))-1)^nu )^(-1)
-  if (lower.tail == TRUE) 
+  cdf <- 1 - ( 1 + (exp((mu*q)^sigma) - 1)^nu )^(-1)
+  
+  if (lower.tail)
     cdf <- cdf
   else cdf <- 1 - cdf
-  if (log.p == FALSE) 
+  if (!log.p) 
     cdf <- cdf
   else cdf <- log(cdf)
   cdf
 }
 #' @export
 #' @rdname dOW
-qOW <- function(p, mu, sigma, nu, lower.tail = TRUE, log.p = FALSE){
+qOW <- function(p, mu, sigma, nu, lower.tail=TRUE, log.p=FALSE){
   if (any(mu<=0 )) 
     stop(paste("mu must be positive", "\n", ""))
   if (any(sigma*nu<=0)) 
     stop(paste("Product sigma*nu must be positive", "\n", ""))
   
-  if (log.p == TRUE) 
+  if (log.p) 
     p <- exp(p)
   else p <- p
-  if (lower.tail == TRUE) 
+  if (lower.tail) 
     p <- p
   else p <- 1 - p
   if (any(p < 0) | any(p > 1)) 
     stop(paste("p must be between 0 and 1", "\n", ""))
-  
-  q <- (1/mu)*(log( 1 + (-1+(1-p)^(-1))^(1/nu) ))^(1/sigma)
+  q <- (1/mu)*(log( 1 + (p*(1-p)^(-1))^(1/nu) ))^(1/sigma)
   q
 }
 #' @importFrom stats runif
@@ -131,7 +123,7 @@ rOW <- function(n, mu, sigma, nu){
 }
 #' @export
 #' @rdname dOW
-hOW<-function(x,mu,sigma,nu){
+hOW<-function(x, mu, sigma, nu){
   if (any(x<0)) 
     stop(paste("x must be positive", "\n", ""))
   if (any(mu<=0 )) 
@@ -139,6 +131,8 @@ hOW<-function(x,mu,sigma,nu){
   if (any(sigma*nu<=0)) 
     stop(paste("Product sigma*nu must be positive", "\n", ""))
   
-  h <- dOW(x, mu, sigma, nu, log = FALSE)/pOW(q = x, mu, sigma, nu, lower.tail=FALSE, log.p = FALSE)
+  h <- dOW(x, mu, sigma, nu, log=FALSE)/pOW(q=x, mu, sigma, nu, 
+                                            lower.tail=FALSE, log.p=FALSE)
   h
 }
+
