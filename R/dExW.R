bcc56716ef507f2f323c95fe93cd9e5fa6c8cfe4
Amylkar <amylkar.urrea@udea.edu.co>
2019-04-05 17:50:47 -0500

    Add dExW distribution :dizzy:

--- R/dExW.R
+++ R/dExW.R
@@ -1,9 +1,9 @@
-#' The Gamma Weibull distribution
+#' The  Extended Weibull distribution
 #' 
 #' @description 
 #' Density, distribution function, quantile function, 
-#' random generation and hazard function for the Gamma Weibull distribution 
-#' with parameters \code{mu}, \code{sigma} and \code{nu} 
+#' random generation and hazard function for the Extended Weibull distribution
+#' with parameters \code{mu}, \code{sigma} and \code{nu}.
 #' 
 #' @param x,q	vector of quantiles.
 #' @param p vector of probabilities.
@@ -15,72 +15,51 @@
 #' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
 #' 
 #' @details 
-#' The Gamma Weibull Distribution with parameters \code{mu}, 
+#' The Extended Weibull distribution with parameters \code{mu}, 
 #' \code{sigma} and \code{nu} has density given by
 #' 
-#' \eqn{f(x) = (\frac{\sigma \mu ^{\nu}}{\tau (\nu)} x^{\nu \sigma - 1}) \exp({-\mu x^{\sigma} }),}
+#' \eqn{f(x) = \frac{\mu \sigma \nu x^{\sigma -1} exp({-\mu x^{\sigma}})} {[1 -(1-\nu) exp({-\mu x^{\sigma}})]^2},}
 #' 
 #' for x > 0. 
 #' 
 #' @return 
-#' \code{dGW} gives the density, \code{pGW} gives the distribution 
-#' function, \code{qGW} gives the quantile function, \code{rGW}
-#' generates random deviates and \code{hGW} gives the hazard function.
+#' \code{dExW} gives the density, \code{pExW} gives the distribution 
+#' function, \code{qExW} gives the quantile function, \code{rExW}
+#' generates random deviates and \code{hExW} gives the hazard function.
 #'
 #' @examples  
 #' ## The probability density function
-#' curve(dGW(x, mu=1, sigma=2, nu=1), from=0.0001, to=4,
+#' curve(dExW(x, mu=0.3, sigma=2, nu=0.05), from=0.0001, to=2,
 #'       col="red", las=1, ylab="f(x)")
 #' 
 #' ## The cumulative distribution and the Reliability function
 #' par(mfrow=c(1, 2))
-#' curve(pGW(x, mu=1, sigma=2, nu=1),
-#'       from=0.0001, to=4, col="red", las=1, ylab="F(x)")
-#' curve(pGW(x, mu=1, sigma=2, nu=1, lower.tail=FALSE),
-#'       from=0.0001, to=4, col="red", las=1, ylab="S(x)")
+#' curve(pExW(x, mu=0.3, sigma=2, nu=0.05),
+#'       from=0.0001, to=2, col="red", las=1, ylab="F(x)")
+#' curve(pExW(x, mu=0.3, sigma=2, nu=0.05, lower.tail=FALSE),
+#'       from=0.0001, to=2, col="red", las=1, ylab="S(x)")
 #' 
 #' ## The quantile function
 #' p <- seq(from=0, to=0.99999, length.out=100)
-#' plot(x=qGW(p, mu=1, sigma=2, nu=1), y=p, xlab="Quantile",
+#' plot(x=qExW(p, mu=0.3, sigma=2, nu=0.05), y=p, xlab="Quantile",
 #'      las=1, ylab="Probability")
-#' curve(pGW(x, mu=1, sigma=2, nu=1), 
-#'       from=0.1, add=TRUE, col="red")
+#' curve(pExW(x, mu=0.3, sigma=2, nu=0.05), 
+#'       from=0, add=TRUE, col="red")
 #' 
 #' ## The random function
-#' hist(rGW(n=10000, mu=1, sigma=2, nu=1), freq=FALSE,
-#'      xlab="x", las=1, main="")
-#' curve(dGW(x, mu=1, sigma=2, nu=1),
-#'       from=0.01, to=4, add=TRUE, col="red")
+#' hist(rExW(n=10000, mu=0.3, sigma=2, nu=0.05), freq=FALSE,
+#'      xlab="x", ylim=c(0, 2), las=1, main="")
+#' curve(dExW(x, mu=0.3, sigma=2, nu=0.05),
+#'       from=0.001, to=4, add=TRUE, col="red")
 #' 
 #' ## The Hazard function
-#' curve(hGW(x, mu=1, sigma=2, nu=1), from=1, to=6,
-#'      col="red", ylab="Hazard function", las=1)
-#'
+#' curve(hExW(x, mu=0.3, sigma=2, nu=0.05), from=0.001, to=4,
+#'       col="red", ylab="Hazard function", las=1)
+#' 
 #' @export
-dGW <- function(x, mu, sigma, nu, log=FALSE){
+dExW <- function(x, mu, sigma, nu, log=FALSE){
   if (any(x <= 0)) 
     stop(paste("x must be positive", "\n", ""))
-  if (any(mu <= 0)) 
-    stop(paste("mu must be positive", "\n", ""))
-  if (any(sigma <= 0)) 
-    stop(paste("sigma must be positive", "\n", ""))
-  if (any(nu <= 0)) 
-    stop(paste("nu must be positive", "\n", "")) 
-  
-  loglik <- log(sigma*mu^(nu)) - log(gamma(nu)) + 
-    (nu*sigma - 1)*log(x) - mu*x^(sigma)
-  
-  if (log == FALSE) 
-    density <- exp(loglik)
-  else 
-    density <- loglik
-  return(density)
-}
-#' @importFrom zipfR Igamma
-#' @export
-#' @rdname dGW
-pGW <- function(q, mu, sigma, nu, 
-                lower.tail=TRUE, log.p=FALSE){
   if (any(mu <= 0)) 
     stop(paste("mu must be positive", "\n", ""))
   if (any(sigma <= 0)) 
@@ -88,9 +67,30 @@ pGW <- function(q, mu, sigma, nu,
   if (any(nu <= 0)) 
     stop(paste("nu must be positive", "\n", "")) 
   
-  t <- mu * q^sigma
-  A <- Igamma(nu, t)
-  cdf <- A / gamma(nu)
+  A <- log(mu) + log(sigma) + log(nu) + (sigma - 1) * log(x) - mu * x^(sigma)
+  B <- 2 * log((1 - (1 - nu) * exp(- mu * x^(sigma))))
+  loglik <- A - B
+  
+  if (log == FALSE) 
+    density <- exp(loglik)
+  else 
+    density <- loglik
+  return(density)
+}
+#' @export
+#' @rdname dExW
+pExW <- function(q, mu, sigma, nu, 
+                 lower.tail=TRUE, log.p=FALSE){
+  if (any(mu <= 0)) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", "")) 
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", "")) 
+  
+  A <- 1 - exp(- mu * q^(sigma))
+  B <- 1 - (1 - nu) * exp(- mu * q^(sigma))
+  cdf <- A /  B
   
   if (lower.tail == TRUE) 
     cdf <- cdf
@@ -101,9 +101,9 @@ pGW <- function(q, mu, sigma, nu,
   cdf
 }
 #' @export
-#' @rdname dGW
-qGW <- function(p, mu, sigma, nu,
-                lower.tail=TRUE, log.p=FALSE){
+#' @rdname dExW
+qExW <- function(p, mu, sigma, nu,
+                 lower.tail=TRUE, log.p=FALSE){
   if (any(mu <= 0)) 
     stop(paste("mu must be positive", "\n", ""))
   if (any(sigma <= 0)) 
@@ -120,10 +120,9 @@ qGW <- function(p, mu, sigma, nu,
     stop(paste("p must be between 0 and 1", "\n", ""))
   
   fda <- function(x, mu, sigma, nu){
-    t <- mu * x^sigma
-    A <- Igamma(nu, t)
-    cdf <- A / gamma(nu)
-    cdf
+    
+    (1 - exp(- mu * x^(sigma))) / ( 1 - (1 - nu) * exp(- mu * x^(sigma)))
+    
   }
   fda1 <- function(x, mu, sigma, nu, p) {
     fda(x, mu, sigma, nu) - p
@@ -137,8 +136,8 @@ qGW <- function(p, mu, sigma, nu,
 }
 #' @importFrom stats runif
 #' @export
-#' @rdname dGW
-rGW <- function(n, mu, sigma, nu){
+#' @rdname dExW
+rExW <- function(n, mu, sigma, nu){
   if (any(mu <= 0)) 
     stop(paste("mu must be positive", "\n", ""))
   if (any(sigma <= 0)) 
@@ -148,12 +147,12 @@ rGW <- function(n, mu, sigma, nu){
   
   n <- ceiling(n)
   p <- runif(n)
-  r <- qGW(p, mu, sigma, nu)
+  r <- qExW(p, mu, sigma, nu)
   r
 }
 #' @export
-#' @rdname dGW
-hGW<-function(x, mu, sigma, nu){
+#' @rdname dExW
+hExW<-function(x, mu, sigma, nu){
   if (any(x <= 0)) 
     stop(paste("x must be positive", "\n", ""))
   if (any(mu <= 0)) 
@@ -163,7 +162,7 @@ hGW<-function(x, mu, sigma, nu){
   if (any(nu <= 0)) 
     stop(paste("nu must be positive", "\n", "")) 
   
-  h <- dGW(x, mu, sigma, nu, log=FALSE) / 
-    pGW(q=x, mu, sigma, nu, lower.tail=FALSE, log.p=FALSE)
+  h <- dExW(x, mu, sigma, nu, log=FALSE) / 
+    pExW(q=x, mu, sigma, nu, lower.tail=FALSE, log.p=FALSE)
   h  
 }
