e59cfeff6a5040aad4c301e61d9f35c0dc1318cf
Jaimemosg <jmosquerag@unal.edu.co>
2019-07-01 10:12:00 -0500

    Package initial message.

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onAttach <- function(libname, pkgname){
+  initmessage <- "<<<<<<<<<<<<<<<<   RelDists Version 1.0.0  >>>>>>>>>>>>>>>>
+  Feel free to report bugs in https://github.com/ousuga/RelDists/issues"
+  packageStartupMessage(initmessage)
+  invisible()
+}
