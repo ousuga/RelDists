72b645016a4488bbcafdc51e79ecfe028f4b1a78
Jaimemosg <jmosquerag@unal.edu.co>
2019-07-01 23:54:33 -0500

    Updated.

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,7 @@
+#Este es un mensaje de prueba (Jaime Mosquera)
 .onAttach <- function(libname, pkgname){
-  initmessage <- "<<<<<<<<<<<<<<<<   RelDists Version 1.0.0  >>>>>>>>>>>>>>>>
-  Feel free to report bugs in https://github.com/ousuga/RelDists/issues"
+  initmessage <- "<<<<<<<<<<<<<<<<<<<<<   RelDists Version 1.0.0  >>>>>>>>>>>>>>>>>>>>>
+Feel free to report bugs in https://github.com/ousuga/RelDists/issues"
   packageStartupMessage(initmessage)
   invisible()
 }
