3220f2b1273500a15a914848211899f1ce824f84
fhernanb <fhernanb@unal.edu.co>
2020-07-16 14:11:46 -0500

    se quitaron todos estos archivos

--- R/zzz.R
+++
@@ -1,7 +0,0 @@
-#Este es un mensaje de prueba (Jaime Mosquera)
-.onAttach <- function(libname, pkgname){
-  initmessage <- "<<<<<<<<<<<<<<<<<<<<<   RelDists Version 1.0.0  >>>>>>>>>>>>>>>>>>>>>
-Feel free to report bugs in https://github.com/ousuga/RelDists/issues"
-  packageStartupMessage(initmessage)
-  invisible()
-}
