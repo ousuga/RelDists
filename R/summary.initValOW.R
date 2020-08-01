8414cb84308294219d1233f6886dbf616e6eb808
Jaimemosg <jmosquerag@unal.edu.co>
2020-08-01 16:09:46 -0500

    Filename changed.

---
+++ R/summary.initValOW.R
@@ -0,0 +1,30 @@
+#' Summary of \code{initValOW} objects
+#' 
+#' @description 
+#' This \code{summary} method displays initial values and search regions
+#' for \code{\link{OW}} family.
+#' 
+#' @aliases summary.initValOW
+#' 
+#' @author Jaime Mosquera Gutiérrez \email{jmosquerag@unal.edu.co}
+#' 
+#' @param object an object of class \code{initVal}, generated with 
+#'               \code{\link{initValuesOW_TTT}}.
+#' @param ... extra arguments
+#'              
+#' @export   
+summary.initValOW <- function(object, ...){
+  cat("--------------------------------------------------------------------\n")
+  cat("Initial Values\n")
+  cat(paste0("sigma = ", object$sigma.start, "\n"))
+  cat(paste0("nu = ", object$nu.start, "\n"))
+  cat("--------------------------------------------------------------------\n")
+  cat("Search Regions\n")
+  cat("For sigma: ")
+  cat(paste0(object$sigma.valid, "\n"))
+  cat("For nu: ")
+  cat(paste0(object$nu.valid, "\n"))
+  cat("--------------------------------------------------------------------\n")
+  cat("Hazard shape: ")
+  cat(object$hazard_type)
+}
