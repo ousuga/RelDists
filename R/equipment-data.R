897fb0e564ef725ca50a861115ddeeb00294e56f
Jaimemosg <jmosquerag@unal.edu.co>
2020-08-02 20:54:56 -0500

    Data for examples added.

---
+++ R/equipment-data.R
@@ -0,0 +1,12 @@
+#' Tensile strengths
+#'
+#' Time to failure in hours of 18 units of the same electronic device.
+#' 
+#' @docType data
+#'
+#' @usage data(equipment)
+#' 
+#' @keywords datasets
+#' 
+#' @format A vector with 18 observations.
+"equipment"
