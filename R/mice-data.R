9534b8be750af69d3b4793bd3fb0964d88193e1d
Jaimemosg <jmosquerag@unal.edu.co>
2020-08-03 10:13:01 -0500

    new dataset added.

---
+++ R/mice-data.R
@@ -0,0 +1,17 @@
+#' Mice mortality data
+#'
+#' The ages at death in weeks for male mice exposed to 240r of gamma radiation.
+#' 
+#' @docType data
+#'
+#' @usage data(mice)
+#' 
+#' @keywords datasets
+#' 
+#' @format A vector with 208 data points.
+#' @importFrom graphics hist lines
+#' @examples
+#' data(mice)
+#' hist(mice, main="", xlab="Time (weeks)", freq=FALSE)
+#' lines(density(mice), col="blue")
+"mice"
