84c35ec93ffdb4756c06510695d7cdc91898d271
Olga Usuga <ousuga@gmail.com>
2020-10-11 21:03:19 -0500

    Add files via upload
    Add distribution

---
+++ R/AddW.R
@@ -0,0 +1,233 @@
+#' The Additive Weibull family
+#' 
+#' @author Amylkar Urrea Montoya, \email{amylkar.urrea@@udea.edu.co}
+#' 
+#' @description 
+#' The Additive Weibull distribution
+#' 
+#' @param mu.link defines the mu.link, with "log" link as the default for the mu parameter.
+#' @param sigma.link defines the sigma.link, with "log" link as the default for the sigma.
+#' @param nu.link defines the nu.link, with "log" link as the default for the nu parameter.
+#' @param tau.link defines the tau.link, with "log" link as the default for the tau parameter. 
+#' 
+#' @seealso \link{dAddW}
+#' 
+#' @details 
+#' Additive Weibull distribution with parameters \code{mu}, 
+#' \code{sigma}, \code{nu} and \code{tau} has density given by
+#' 
+#' \eqn{f(x) = (\mu\nu x^{\nu - 1} + \sigma\tau x^{\tau - 1}) \exp({-\mu x^{\nu} - \sigma x^{\tau} }),}
+#' 
+#' for x > 0. 
+#' 
+#' @examples 
+#' # Example 1
+#' # Generating some random values with
+#' # known mu, sigma, nu and tau
+#' y <- rAddW(n=100, mu=1.5, sigma=0.2, nu=3, tau=0.8)
+#' 
+#' # Fitting the model
+#' require(gamlss)
+#' 
+#' mod <- gamlss(y~1, sigma.fo=~1, nu.fo=~1, tau.fo=~1, family='AddW',
+#'               control=gamlss.control(n.cyc=5000, trace=FALSE))
+#' 
+#' # Extracting the fitted values for mu, sigma, nu and tau
+#' # using the inverse link function
+#' exp(coef(mod, what='mu'))
+#' exp(coef(mod, what='sigma'))
+#' exp(coef(mod, what='nu'))
+#' exp(coef(mod, what='tau'))
+#' 
+#' # Example 2
+#' # Generating random values under some model
+#' n <- 200
+#' x1 <- runif(n, min=0.4, max=0.6)
+#' x2 <- runif(n, min=0.4, max=0.6)
+#' mu <- exp(1.67 + -3 * x1)
+#' sigma <- exp(0.69 - 2 * x2)
+#' nu <- 3
+#' tau <- 0.8
+#' x <- rAddW(n=n, mu, sigma, nu, tau)
+#' 
+#' mod <- gamlss(x~x1, sigma.fo=~x2, nu.fo=~1, tau.fo=~1, family=AddW,
+#'               control=gamlss.control(n.cyc=5000, trace=FALSE))
+#' 
+#' coef(mod, what="mu")
+#' coef(mod, what="sigma")
+#' exp(coef(mod, what="nu"))
+#' exp(coef(mod, what="tau"))
+#' 
+#' @references
+#' \insertRef{almalki2014modifications}{RelDists}
+#' 
+#' \insertRef{Xie1996}{RelDists}
+#'
+#' @importFrom Rdpack reprompt
+#' @importFrom gamlss.dist checklink
+#' @importFrom gamlss rqres.plot
+#' @export
+AddW <- function (mu.link="log", sigma.link="log", nu.link="log", tau.link="log") {
+  mstats <- checklink("mu.link", "Additive Weibull", 
+                      substitute(mu.link), c("log", "own"))
+  dstats <- checklink("sigma.link", "Additive Weibull",
+                      substitute(sigma.link), c("log", "own"))
+  vstats <- checklink("nu.link", "Additive Weibull", 
+                      substitute(nu.link), c("log", "own"))
+  tstats <- checklink("tau.link", "Additive Weibull", 
+                      substitute(tau.link), c("log", "own"))
+  
+  structure(list(family=c("AddW", "Additive Weibull"), 
+                 parameters=list(mu=TRUE, sigma=TRUE, nu=TRUE, tau=TRUE), 
+                 nopar=4, 
+                 type="Continuous", 
+                 
+                 mu.link = as.character(substitute(mu.link)), 
+              sigma.link = as.character(substitute(sigma.link)), 
+                 nu.link = as.character(substitute(nu.link)),
+                tau.link = as.character(substitute(tau.link)),
+                 
+              mu.linkfun = mstats$linkfun, 
+           sigma.linkfun = dstats$linkfun, 
+              nu.linkfun = vstats$linkfun,
+             tau.linkfun = tstats$linkfun,
+                 
+              mu.linkinv = mstats$linkinv, 
+           sigma.linkinv = dstats$linkinv, 
+              nu.linkinv = vstats$linkinv,
+             tau.linkinv = tstats$linkinv,
+                 
+                   mu.dr = mstats$mu.eta, 
+                sigma.dr = dstats$mu.eta, 
+                   nu.dr = vstats$mu.eta,
+                  tau.dr = tstats$mu.eta,
+                 
+           # Primeras derivadas ---------------------------------
+           dldm = function(y, mu, sigma, nu, tau) {
+             A    <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldm <- (nu * y^(nu - 1))/A - y^nu
+             dldm
+           },
+           
+           dldd = function(y, mu, sigma, nu, tau) {
+             A    <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldd <- (tau * y^(tau - 1))/A - y^tau
+             dldd
+           },
+           
+           dldv = function(y, mu, sigma, nu, tau) {
+             A    <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             B    <- y^(nu - 1) + nu * log(y) * y^(nu - 1)
+             dldv <- (mu * B)/A - mu * log(y) * y^nu 
+             dldv
+           },
+           
+           dldt = function(y, mu, sigma, nu, tau) {
+             A    <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             B    <- y^(tau - 1) + tau * log(y) * y^(tau - 1)
+             dldt <- (sigma * B)/A - sigma * log(y) * y^tau 
+             dldt
+           },
+           
+           # Segundas derivadas ---------------------------------
+           d2ldm2 = function(y, mu, sigma, nu, tau) {
+             A      <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldm   <- (nu * y^(nu - 1))/A - y^nu
+             d2ldm2 <- -dldm * dldm
+             d2ldm2
+           },
+           
+           d2ldmdd = function(y, mu, sigma, nu, tau) {
+             A       <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldm    <- (nu * y^(nu - 1))/A - y^nu
+             dldd    <- (tau * y^(tau - 1))/A - y^tau
+             d2ldmdd <- -dldm * dldd
+             d2ldmdd
+           },
+           
+           d2ldmdv = function(y, mu, sigma, nu, tau) {
+             A       <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldm    <- (nu * y^(nu - 1))/A - y^nu
+             B       <- y^(nu - 1) + nu * log(y) * y^(nu - 1)
+             dldv    <- (mu * B)/A - mu * log(y) * y^nu 
+             d2ldmdv <- -dldm * dldv
+             d2ldmdv
+           },
+           
+           d2ldmdt = function(y, mu, sigma, nu, tau) {
+             A       <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldm    <- (nu * y^(nu - 1))/A - y^nu
+             B       <- y^(tau - 1) + tau * log(y) * y^(tau - 1)
+             dldt    <- (sigma * B)/A - sigma * log(y) * y^tau 
+             d2ldmdt <- -dldm * dldt
+             d2ldmdt
+           },
+           
+           d2ldd2 = function(y, mu, sigma, nu, tau) {
+             A      <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldd   <- (tau * y^(tau - 1))/A - y^tau
+             d2ldd2 <- -dldd * dldd
+             d2ldd2
+           },
+           
+           d2ldddv = function(y, mu, sigma, nu, tau) {
+             A       <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldd    <- (tau * y^(tau - 1))/A - y^tau
+             B       <- y^(nu - 1) + nu * log(y) * y^(nu - 1)
+             dldv    <- (mu * B)/A - mu * log(y) * y^nu 
+             d2ldddv <- -dldd * dldv
+             d2ldddv
+           },
+           
+           d2ldddt = function(y, mu, sigma, nu, tau) {
+             A       <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             dldd    <- (tau * y^(tau - 1))/A - y^tau
+             B       <- y^(tau - 1) + tau * log(y) * y^(tau - 1)
+             dldt    <- (sigma * B)/A - sigma * log(y) * y^tau 
+             d2ldddt <- -dldd * dldt
+             d2ldddt
+           },
+           
+           d2ldv2 = function(y, mu, sigma, nu, tau) {
+             A      <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             B      <- y^(nu - 1) + nu * log(y) * y^(nu - 1)
+             dldv   <- (mu * B)/A - mu * log(y) * y^nu 
+             d2ldv2 <- -dldv * dldv
+             d2ldv2
+           },
+           
+           d2ldvdt = function(y, mu, sigma, nu, tau) {
+             A       <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             B       <- y^(nu - 1) + nu * log(y) * y^(nu - 1)
+             dldv    <- (mu * B)/A - mu * log(y) * y^nu 
+             C       <- y^(tau - 1) + tau * log(y) * y^(tau - 1)
+             dldt    <- (sigma * C)/A - sigma * log(y) * y^tau 
+             d2ldvdt <- -dldv * dldt
+             d2ldvdt
+           },
+           
+           d2ldt2 = function(y, mu, sigma, nu, tau) {
+             A      <- mu * nu * y^(nu - 1) + sigma * tau * y^(tau - 1)
+             B      <- y^(tau - 1) + tau * log(y) * y^(tau - 1)
+             dldt   <- (sigma * B)/A - sigma * log(y) * y^tau 
+             d2ldt2 <- -dldt * dldt
+             d2ldt2
+           },
+           
+            G.dev.incr = function(y, mu, sigma, nu, tau, ...) -2*dAddW(y, mu, sigma, nu, tau, log=TRUE), 
+                 rqres = expression(rqres(pfun="pAddW", type="Continuous", y=y, mu=mu, sigma=sigma, nu=nu, tau=tau)), 
+                 
+            mu.initial = expression(mu    <- rep(1, length(y))), 
+         sigma.initial = expression(sigma <- rep(1, length(y))), 
+            nu.initial = expression(nu    <- rep(1, length(y))),
+           tau.initial = expression(tau    <- rep(1, length(y))),
+                 
+              mu.valid = function(mu)    all(mu > 0), 
+           sigma.valid = function(sigma) all(sigma > 0), 
+              nu.valid = function(nu)    all(nu > 0),
+             tau.valid = function(tau)    all(tau > 0),
+                 
+               y.valid = function(y) all(y > 0)
+  ), 
+  class=c("gamlss.family", "family"))
+}
---
+++ R/dAddW.R
@@ -0,0 +1,187 @@
+#' The Additive Weibull distribution
+#' 
+#' @author Amylkar Urrea Montoya, \email{amylkar.urrea@@udea.edu.co}
+#' 
+#' @description 
+#' Density, distribution function, quantile function, 
+#' random generation and hazard function for the Additive Weibull distribution
+#' with parameters \code{mu}, \code{sigma}, \code{nu} and \code{tau}.
+#' 
+#' @param x,q	vector of quantiles.
+#' @param p vector of probabilities.
+#' @param n number of observations. 
+#' @param mu parameter.
+#' @param sigma parameter.
+#' @param nu parameter.
+#' @param tau parameter.
+#' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
+#' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
+#' 
+#' @details 
+#' Additive Weibull Distribution with parameters \code{mu}, 
+#' \code{sigma}, \code{nu} and \code{tau} has density given by
+#' 
+#' \eqn{f(x) = (\mu\nu x^{\nu - 1} + \sigma\tau x^{\tau - 1}) \exp({-\mu x^{\nu} - \sigma x^{\tau} }),}
+#' 
+#' for x > 0. 
+#' 
+#' @return 
+#' \code{dAddW} gives the density, \code{pAddW} gives the distribution 
+#' function, \code{qAddW} gives the quantile function, \code{rAddW}
+#' generates random deviates and \code{hAddW} gives the hazard function.
+#'
+#' @examples  
+#' ## The probability density function
+#' curve(dAddW(x, mu=1.5, sigma=0.5, nu=3, tau=0.8), from=0.0001, to=2,
+#'       col="red", las=1, ylab="f(x)")
+#' 
+#' ## The cumulative distribution and the Reliability function
+#' par(mfrow=c(1, 2))
+#' curve(pAddW(x, mu=1.5, sigma=0.5, nu=3, tau=0.8),
+#'       from=0.0001, to=2, col="red", las=1, ylab="F(x)")
+#' curve(pAddW(x, mu=1.5, sigma=0.5, nu=3, tau=0.8, lower.tail=FALSE),
+#'      from=0.0001, to=2, col="red", las=1, ylab="S(x)")
+#' 
+#' ## The quantile function
+#' p <- seq(from=0, to=0.99999, length.out=100)
+#' plot(x=qAddW(p, mu=1.5, sigma=0.2, nu=3, tau=0.8), y=p, xlab="Quantile",
+#'      las=1, ylab="Probability")
+#' curve(pAddW(x, mu=1.5, sigma=0.2, nu=3, tau=0.8), 
+#'       from=0, add=TRUE, col="red")
+#' 
+#' ## The random function
+#' hist(rAddW(n=10000, mu=1.5, sigma=0.2, nu=3, tau=0.8), freq=FALSE,
+#'      xlab="x", las=1, main="")
+#' curve(dAddW(x, mu=1.5, sigma=0.2, nu=3, tau=0.8),
+#'       from=0.09, to=5, add=TRUE, col="red")
+#' 
+#' ## The Hazard function
+#' curve(hAddW(x, mu=1.5, sigma=0.2, nu=3, tau=0.8), from=0.001, to=1,
+#'       col="red", ylab="Hazard function", las=1)
+#'
+#' @references
+#' \insertRef{almalki2014modifications}{RelDists}
+#' 
+#' \insertRef{Xie1996}{RelDists}
+#'
+#' @importFrom Rdpack reprompt
+#'
+#' @export
+dAddW <- function(x, mu, sigma,
+                 nu, tau, log=FALSE){
+  if (any(x <= 0)) 
+    stop(paste("x must be positive", "\n", ""))
+  if (any(mu <= 0)) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", "")) 
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", "")) 
+  if (any(tau <= 0)) 
+    stop(paste("tau must be postive", "\n", "")) 
+  
+  A <- mu*nu*x^(nu-1)
+  B <- sigma*tau*x^(tau-1)
+  loglik <- log(A+B) - (mu*x^(nu) + sigma*x^(tau))
+  
+  if (log == FALSE) 
+    density <- exp(loglik)
+  else 
+    density <- loglik
+  return(density)
+}
+#' @export
+#' @rdname dAddW
+pAddW <- function(q, mu, sigma, nu, tau, 
+                 lower.tail=TRUE, log.p=FALSE){
+  if (any(mu <= 0)) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", "")) 
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", "")) 
+  if (any(tau <= 0)) 
+    stop(paste("tau must be postive", "\n", "")) 
+  
+  cdf <- 1 - exp(-mu*q^(nu) - sigma*q^(tau))
+  
+  if (lower.tail == TRUE) 
+    cdf <- cdf
+  else cdf <- 1 - cdf
+  if (log.p == FALSE) 
+    cdf <- cdf
+  else cdf <- log(cdf)
+  cdf
+}
+#' @export
+#' @rdname dAddW
+qAddW <- function(p, mu, sigma, nu, tau,
+                 lower.tail=TRUE, log.p=FALSE){
+  if (any(mu <= 0)) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", "")) 
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", "")) 
+  if (any(tau <= 0)) 
+    stop(paste("tau must be postive", "\n", "")) 
+  
+  if (log.p == TRUE) 
+    p <- exp(p)
+  else p <- p
+  if (lower.tail == TRUE) 
+    p <- p
+  else p <- 1 - p
+  if (any(p < 0) | any(p > 1)) 
+    stop(paste("p must be between 0 and 1", "\n", ""))
+  
+  fda <- function(x, mu, sigma, nu, tau){
+    1 - exp(-mu*x^(nu)-sigma*x^(tau))
+  }
+  fda1 <- function(x, mu, sigma, nu, tau, p) {
+    fda(x, mu, sigma, nu, tau) - p
+  }
+  r_de_la_funcion <- function(mu, sigma, nu, tau, p) {
+    uniroot(fda1, interval=c(0, 1e+06), mu, sigma, nu, tau, p)$root
+  }
+  r_de_la_funcion <- Vectorize(r_de_la_funcion)
+  q <- r_de_la_funcion(mu, sigma, nu, tau, p)
+  q
+}
+#' @importFrom stats runif
+#' @export
+#' @rdname dAddW
+rAddW <- function(n, mu, sigma, nu, tau){
+  if (any(mu <= 0)) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", ""))
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", "")) 
+  if (any(tau <= 0)) 
+    stop(paste("tau must be postive", "\n", "")) 
+  
+  n <- ceiling(n)
+  p <- runif(n)
+  r <- qAddW(p, mu, sigma, nu, tau)
+  r
+}
+#' @export
+#' @rdname dAddW
+hAddW <- function(x, mu, sigma, nu, tau){
+  if (any(x <= 0)) 
+    stop(paste("x must be positive", "\n", ""))
+  if (any(mu <= 0)) 
+    stop(paste("mu must be positive", "\n", ""))
+  if (any(sigma <= 0)) 
+    stop(paste("sigma must be positive", "\n", ""))  
+  if (any(nu <= 0)) 
+    stop(paste("nu must be positive", "\n", "")) 
+  if (any(tau <= 0)) 
+    stop(paste("tau must be postive", "\n", ""))
+  
+  h <- dAddW(x, mu, sigma, nu, tau, log=FALSE) / 
+    pAddW(q=x, mu, sigma, nu, tau, lower.tail=FALSE, log.p=FALSE)
+  h  
+}
+
