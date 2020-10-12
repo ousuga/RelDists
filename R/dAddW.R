dede1ee043c97e6987b8c73038fed2d5726c8412
fhernanb <fhernanb@unal.edu.co>
2020-10-12 11:40:51 -0500

    Ejemplo de Add

--- R/dAddW.R
+++ R/dAddW.R
@@ -12,8 +12,8 @@
 #' @param n number of observations. 
 #' @param mu parameter.
 #' @param sigma parameter.
-#' @param nu parameter.
-#' @param tau parameter.
+#' @param nu shape parameter.
+#' @param tau shape parameter.
 #' @param log,log.p	logical; if TRUE, probabilities p are given as log(p).	
 #' @param lower.tail logical; if TRUE (default), probabilities are P[X <= x], otherwise, P[X > x].
 #' 
