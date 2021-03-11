1d2a5ce8c89b6212c1a01259790d5a41a94cc3c8
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-10 20:37:32 -0600

    Example updated.

--- examples/examples_initValuesOW_TTT.R
+++ examples/examples_initValuesOW_TTT.R
@@ -1,10 +1,10 @@
 # Example 1
 # Bathtuh hazard and its corresponding TTT plot
-y1 <- rOW(n=50, mu=0.022, sigma=8, nu=0.01)
+y1 <- rOW(n = 100, mu = 0.1, sigma = 7, nu = 0.08)
 my_initial_guess <- initValuesOW_TTT(formula=y1~1)
 summary(my_initial_guess)
-plot(my_initial_guess, legend_options=list(pos=1.04),
-     par_plot=list(mar=c(3.7,3.7,1,1.5), mgp=c(2.5,1,0)))
+plot(my_initial_guess, par_plot=list(mar=c(3.7,3.7,1,2),
+                                     mgp=c(2.5,1,0)))
 
 curve(hOW(x, mu = 0.022, sigma = 8, nu = 0.01), from = 0, 
       to = 80, ylim = c(0, 0.04), col = "red", 
