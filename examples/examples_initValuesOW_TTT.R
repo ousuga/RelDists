96d95189da2b6d3c93d2caf18ea6ba8dc8bf8f06
Jaimemosg <jmosquerag@unal.edu.co>
2021-03-31 17:35:12 -0500

    Bugs in example fixed.

--- examples/examples_initValuesOW_TTT.R
+++ examples/examples_initValuesOW_TTT.R
@@ -2,7 +2,7 @@
 # Bathtuh hazard and its corresponding TTT plot
 y1 <- rOW(n = 1000, mu = 0.1, sigma = 7, nu = 0.08)
 my_initial_guess1 <- initValuesOW_TTT(formula=y1~1)
-summary(my_initial_guess)
+summary(my_initial_guess1)
 plot(my_initial_guess1, par_plot=list(mar=c(3.7,3.7,1,2.5),
                                      mgp=c(2.5,1,0)))
 
@@ -16,7 +16,7 @@ y2 <- rOW(n = 1000, mu = 0.1, sigma = 7, nu = 0.08)
 status <- c(rep(1, 990), rep(0, 10))
 my_initial_guess2 <- initValuesOW_TTT(formula=Surv(y2, status)~1)
 summary(my_initial_guess2)
-plot(my_initial_guess, par_plot=list(mar=c(3.7,3.7,1,2.5),
+plot(my_initial_guess2, par_plot=list(mar=c(3.7,3.7,1,2.5),
                                      mgp=c(2.5,1,0)))
 
 curve(hOW(x, mu = 0.022, sigma = 8, nu = 0.01), from = 0, 
