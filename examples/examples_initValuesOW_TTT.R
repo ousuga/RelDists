c89158561b0bf3474bba1f8d5d387fddfe3f0ed0
Jaimemosg <jmosquerag@unal.edu.co>
2021-04-01 01:09:05 -0500

    dontrun command.

--- examples/examples_initValuesOW_TTT.R
+++ examples/examples_initValuesOW_TTT.R
@@ -12,6 +12,7 @@ curve(hOW(x, mu = 0.022, sigma = 8, nu = 0.01), from = 0,
 
 # Example 2
 # Bathtuh hazard and its corresponding TTT plot with right censored data
+\dontrun{
 y2 <- rOW(n = 1000, mu = 0.1, sigma = 7, nu = 0.08)
 status <- c(rep(1, 980), rep(0, 20))
 my_initial_guess2 <- initValuesOW_TTT(formula=Surv(y2, status)~1)
@@ -22,3 +23,4 @@ plot(my_initial_guess2, par_plot=list(mar=c(3.7,3.7,1,2.5),
 curve(hOW(x, mu = 0.022, sigma = 8, nu = 0.01), from = 0, 
       to = 80, ylim = c(0, 0.04), col = "red", 
       ylab = "Hazard function", las = 1)
+}
