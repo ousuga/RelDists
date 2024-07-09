90946a904a4d22f64fc3b4320064df174f4e5c66
fhernanb <fhernanb@unal.edu.co>
2024-07-09 17:39:32 -0500

    new KumIW distribution

--- examples/examples_KumIW.R
+++ examples/examples_KumIW.R
@@ -1,31 +1,31 @@
 # Example 1
 # Generating some random values with
-# known mu, sigma, nu and tau
-y <- rKumIW(n=1000, mu = 1.5, sigma=  1.5, nu = 5)
+# known mu, sigma and nu
+y <- rKumIW(n=100, mu=1.5, sigma=2.3, nu=1)
 
 # Fitting the model
 require(gamlss)
 
-mod <- gamlss(y~1, sigma.fo=~1, nu.fo=~1, family='KumIW',
+mod <- gamlss(y~1, sigma.fo=~1, nu.fo=~1, family=KumIW,
               control=gamlss.control(n.cyc=5000, trace=FALSE))
 
 # Extracting the fitted values for mu, sigma and nu 
 # using the inverse link function
-exp(coef(mod, what='mu'))
-exp(coef(mod, what='sigma'))
-exp(coef(mod, what='nu'))
+exp(coef(mod, what="mu"))
+exp(coef(mod, what="sigma"))
+exp(coef(mod, what="nu"))
 
 # Example 2
 # Generating random values under some model
 n <- 200
-x1 <- runif(n, min=0.4, max=0.6)
-x2 <- runif(n, min=0.4, max=0.6)
-mu <- exp(1 - x1)
-sigma <- exp(1 - x2)
+x1 <- runif(n)
+x2 <- runif(n)
+mu <- exp(1 +  -1 * x1)
+sigma <- exp(1 + -1 * x2)
 nu <- 5
-x <- rKumIW(n=n, mu, sigma, nu)
+y <- rKumIW(n=n, mu=mu, sigma=sigma, nu=nu)
 
-mod <- gamlss(x~x1, sigma.fo=~x2, nu.fo=~1, family=KumIW,
+mod <- gamlss(y~x1, sigma.fo=~x2, nu.fo=~1, family=KumIW,
               control=gamlss.control(n.cyc=5000, trace=FALSE))
 
 coef(mod, what="mu")
