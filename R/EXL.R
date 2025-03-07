b46a852a6f54b4a99c3dce4b719fe90ae6687cde
fhernanb <fhernanb@unal.edu.co>
2025-03-07 10:16:21 -0500

    I removed .lintr file temporaly

--- R/EXL.R
+++ R/EXL.R
@@ -3,15 +3,20 @@
 #' @author Manuel Gutierrez Tangarife, \email{mgutierrezta@unal.edu.co}
 #' 
 #' @description 
-#' The function \code{EXL()} defines The exponentiated XLindley, a two parameter 
-#' distribution, for a \code{gamlss.family} object to be used in GAMLSS fitting 
+#' The function \code{EXL()} defines The exponentiated XLindley, 
+#' a two parameter distribution, for a \code{gamlss.family} object 
+#' to be used in GAMLSS fitting 
 #' using the function \code{gamlss()}.
 #' 
-#' @param mu.link defines the mu.link, with "log" link as the default for the mu parameter.
-#' @param sigma.link defines the sigma.link, with "log" link as the default for the sigma.
+#' @param mu.link defines the mu.link, with "log" link as the default 
+#' for the mu parameter.
+#' @param sigma.link defines the sigma.link, with "log" link as the default 
+#' for the sigma.
 #' 
 #' @references
-#' Alomair, A. M., Ahmed, M., Tariq, S., Ahsan-ul-Haq, M., & Talib, J. (2024). An exponentiated XLindley distribution with properties, inference and applications. Heliyon, 10(3).
+#' Alomair, A. M., Ahmed, M., Tariq, S., Ahsan-ul-Haq, M., & Talib, J. 
+#' (2024). An exponentiated XLindley distribution with properties, 
+#' inference and applications. Heliyon, 10(3).
 #' 
 #' @seealso \link{EXL}.
 #' 
@@ -23,18 +28,19 @@
 #'  \left(1+\frac{\mu x}{(1 + \mu)^2}\right) \exp(-\mu x)\right] ^ {\sigma-1} }
 #' for \eqn{x \geq 0}, \eqn{\mu \geq 0} and \eqn{\sigma \geq 0}.
 #' 
-#' Note: In this implementation we changed the original parameters \eqn{\delta} for \eqn{\mu}
-#' and \eqn{\alpha} for \eqn{\sigma} we did it to implement this distribution
-#' within gamlss framework.
+#' Note: In this implementation we changed the original parameters 
+#' \eqn{\delta} for \eqn{\mu} and \eqn{\alpha} for \eqn{\sigma} we 
+#' did it to implement this distribution within gamlss framework.
 #' 
-#' @returns Returns a gamlss.family object which can be used to fit a EXL distribution in the \code{gamlss()} function.
+#' @returns Returns a gamlss.family object which can be used to fit a 
+#' EXL distribution in the \code{gamlss()} function.
 #' 
 #' @example examples/examples_EXL.R
 #' 
 #' @importFrom gamlss.dist checklink
 #' @importFrom gamlss rqres.plot
 #' @export
-EXL <- function (mu.link="log", sigma.link="log") {
+EXL <- function(mu.link="log", sigma.link="log") {
   mstats <- checklink("mu.link", "Exponentiated XLindley", substitute(mu.link), c("log", "identity"))
   dstats <- checklink("sigma.link", "Exponentiated XLindley", substitute(sigma.link), c("log", "identity"))
   
@@ -110,14 +116,12 @@ EXL <- function (mu.link="log", sigma.link="log") {
                  G.dev.incr = function(y, mu, sigma, ...) -2*dEXL(y, mu, sigma, log=TRUE), 
                  rqres = expression(rqres(pfun="pEXL", type="Continuous", y=y, mu=mu, sigma=sigma)),
                  
-                 #mu.initial    = expression(    mu <- rep(initValuesEXL(y)[1], length(y)) ),     
-                 #sigma.initial = expression( sigma <- rep(initValuesEXL(y)[2], length(y)) ), 
                  mu.initial    = expression(    mu <- rep(1, length(y)) ),     
                  sigma.initial = expression( sigma <- rep(1, length(y)) ), 
                  
-                 mu.valid = function(mu) all(mu >= 0) , 
+                 mu.valid = function(mu) all(mu >= 0), 
                  sigma.valid = function(sigma)  all(sigma > 0), 
                  y.valid = function(y)  all(y >= 0)
   ),
-  class = c("gamlss.family","family"))
+  class = c("gamlss.family", "family"))
 }
